f43b2f3ff1c73d41ed44126dc341572f6cfe5858
Nasim Mahmud <nasim.mishu@northsouth.edu>
2022-09-27 21:25:20 +0600

    Initializing

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ import requests
 response = requests.get(url="https://www.empireonline.com/movies/features/best-movies-2")
 
 empire_webpage = response.text
-
+soup = BeautifulSoup(empire_webpage, "html.parser")
