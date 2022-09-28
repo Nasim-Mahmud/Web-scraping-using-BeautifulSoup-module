f79db156a8a272d0ad6f662901a25a0b405e4a5d
Nasim Mahmud <nasim.mishu@northsouth.edu>
2022-09-28 07:43:31 +0600

    Initializing

--- main.py
+++ main.py
@@ -6,14 +6,14 @@ response = requests.get(url="https://www.imdb.com/list/ls055592025/")
 imdb_webpage = response.text
 soup = BeautifulSoup(imdb_webpage, "html.parser")
 
-movies = soup.find_all(name="a", class_="/title")
-print(movies)
+movies = soup.find_all(name="h3", class_="lister-item-header")
+# print(movies)
 
 movie_list = []
 for movie in movies:
-    # item = movie.find_all("a")
-    items = movie.get_text("href")
-    # movie_list.append(item)
-    print(items)
+    item = movie.find_all("a")
+    # items = movie.get_text()
+    movie_list.append(item)
 
-# print(movie_list)
+
+print(movie_list)
