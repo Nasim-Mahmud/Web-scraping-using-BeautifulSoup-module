0940ae5e2b5c45cdcc488b6561f7368c199eebbb
Nasim Mahmud <nasim.mishu@northsouth.edu>
2022-09-28 08:36:55 +0600

    Initializing

--- main.py
+++ main.py
@@ -7,17 +7,8 @@ imdb_webpage = response.text
 soup = BeautifulSoup(imdb_webpage, "html.parser")
 
 movies = soup.find_all(name="h3", class_="lister-item-header")
-# print(movies)
-
-# movie_list = []
-# for movie in movies:
-#     item = movie.find_all("a")
-#     for i in item:
-#         movie_list.append(i)
-#     # items = movie.get_text()
 
 movie_titles = [movie.getText().replace("\n", "") for movie in movies]
-print(len(movie_titles))
 
 with open("movies.txt", mode="w") as file:
     for n in range(len(movie_titles)):
