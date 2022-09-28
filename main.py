7408f61d2590ba2cd13738922304a4f3801762a7
Nasim Mahmud <nasim.mishu@northsouth.edu>
2022-09-28 07:59:05 +0600

    Initializing

--- main.py
+++ main.py
@@ -9,11 +9,14 @@ soup = BeautifulSoup(imdb_webpage, "html.parser")
 movies = soup.find_all(name="h3", class_="lister-item-header")
 # print(movies)
 
-movie_list = []
-for movie in movies:
-    item = movie.find_all("a")
-    # items = movie.get_text()
-    movie_list.append(item)
+# movie_list = []
+# for movie in movies:
+#     item = movie.find_all("a")
+#     for i in item:
+#         movie_list.append(i)
+#     # items = movie.get_text()
+
+movie_titles = [movie.getText() for movie in movies]
+print(movie_titles)
 
 
-print(movie_list)
