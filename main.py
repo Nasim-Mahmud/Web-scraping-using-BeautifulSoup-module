be4c28710bd94420fdbb26f4c3b11c13cd8028e3
Nasim Mahmud <nasim.mishu@northsouth.edu>
2022-09-28 07:20:53 +0600

    Initializing

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from bs4 import BeautifulSoup
 import requests
 
-response = requests.get(url="https://www.empireonline.com/movies/features/best-movies-2")
+response = requests.get(url="https://www.imdb.com/list/ls055592025/")
 
-empire_webpage = response.text
-soup = BeautifulSoup(empire_webpage, "html.parser")
+imdb_webpage = response.text
+soup = BeautifulSoup(imdb_webpage, "html.parser")
 
-data = soup.find_all(name="h3", class_="jsx-4245974604")
-print(data)
+movie_list = soup.find_all(name="h3", class_="lister-item-header")
+for movie in movie_list:
